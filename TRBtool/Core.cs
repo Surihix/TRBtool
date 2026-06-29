6a88c177520813e1fcb4c24ca848ccc192b4accf
Surihix <43111452+Surihix@users.noreply.github.com>
2026-06-29 13:48:34 +0530

    fixed wpd_imgb repacking for r8g8b8a8 format

--- TRBtool/Core.cs
+++ TRBtool/Core.cs
@@ -32,7 +32,7 @@ namespace TRBtool
                         var hashArray = dllHash.ComputeHash(dllStream);
                         var computedHash = BitConverter.ToString(hashArray).Replace("-", "").ToLower();
 
-                        if (computedHash != "f53440eccf80116163214babad6ba722505deb04b805fa1926e8b59cd5d2fac6")
+                        if (computedHash != "285847a4f1515bca78d7ab0b7d78c0042acb4c92f3a651046ff9d7bef9101265")
                         {
                             SharedMethods.ErrorExit("Error: 'IMGBlibrary.dll' file is corrupt. please check if the dll file is valid.");
                         }
